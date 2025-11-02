0eeae4af25f9fc1ceceae1b91b4f8b636e46231d
carlosmartins1985 <carlostenzel@gmail.com>
2025-11-02 13:43:55 -0300

    feat: add initial header component

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-
-  return <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim facilis vero magnam, iure ipsa velit! Asperiores magnam praesentium atque, harum fugit, error ducimus quibusdam sapiente, amet vero dolorum! Perferendis, dignissimos?</p>;
-  
-}
